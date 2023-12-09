b1b6a2ef1c15c621a0719715887efbac465b30cc
Bum <username@users.noreply.github.com>
2023-12-08 23:49:13 -1000

    advent 2022 day_16 understanding

--- advent/2022/16/day16.py
+++ advent/2022/16/day16.py
@@ -1,3 +1,16 @@
+"""
+Elephants have a hand-held device that sends a distress beacon.
+
+We need to get the elephants out of the cave, there is 30 minutes to escape
+
+You can open a system of tunnels with valves that opens pipes
+- it takes a minute to open the valves and another minute to move to a new valve
+- All valves are closed
+- You and the elephants start at valve AA
+- the minutes-left * flow-rate = pressure_release
+
+Find out how to release as much pressure as possible.
+"""
 def p1(filename):
     for line in open(filename):
         print(line)
