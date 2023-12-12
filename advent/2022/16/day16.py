e3798b73578c15bfbdc22757a41cd63e44c95268
Bum <username@users.noreply.github.com>
2023-12-12 11:34:08 -1000

    start debugging day16 part 1

--- advent/2022/16/day16.py
+++ advent/2022/16/day16.py
@@ -17,6 +17,24 @@ references:
     - https://www.youtube.com/watch?v=bLMj50cpOug&list=PLnNm9syGLD3yf-YW-a5XNh1CJN07xr0Kz&index=16
         a video explaining how to solve the problem. 
 """
+from collections import deque
+
+def dfs (time, valve, bitmask, cache):
+    if (time, valve, bitmask) in cache:
+        return cache[(time, valve, bitmask)]
+
+    maxval = 0
+    for neighbor in dists[valve]:
+        bit = 1 << indices[neighbor]
+        if bitmask & bit:
+            continue
+        remtime = time - dists[valve][neighbor] - 1
+        if remtime <= 0:
+            continue
+        maxval = max(maxval, dfs(remtime, neighbor, bitmask | bit, cache) + valves[neighbor] * remtime)
+        cache[(time, valve, bitmask)] = maxval
+        return maxval
+
 def p1(filename):
     # dictionaries
     valves = {}
@@ -32,7 +50,47 @@ def p1(filename):
         valves[valve] = flow
         tunnels[valve] = targets
 
+    dists = {}
+    nonempty = []
+    for valve in valves:
+        if valve != "AA" and not valves[valve]:
+            continue
+        if valve != "AA":
+            nonempty.append(valve)
+
+        dists[valve] = {valve: 0, "AA": 0}
+        visted = {valve}
+
+        queue = deque([0, valve])
+        # problem with queue.popleft() 12/12/23
+        # Traceback (most recent call last):
+        # File "C:\Users\shita\Projects\code\advent\2022\16\day16.py", line 95, in <module>
+        #   p1("example")
+        # File "C:\Users\shita\Projects\code\advent\2022\16\day16.py", line 67, in p1
+        #   distance, position = queue.popleft()
+        #   ^^^^^^^^^^^^^^^^^^
+        while queue:
+            distance, position = queue.popleft()
+            for neighbor in tunnels[position]:
+                if neighbor in visted:
+                    continue
+                visted.add(neighbor)
+                if valves[neighbor]:
+                    dists[valve][neighbor] = distance + 1
+                queue.append((distance + 1, neighbor))
+
+        del dists[valve][valve]
+        if valve != "AA":
+            del dists[valve]["AA"]
+
+    indices = {}
+    for index, element in enumerate(nonempty):
+        indices[element] = index
+
+    cache = {}
+
     print("Answer to part 1: ")
+    print(dfs(30, "AA", 0, cache))
 
 def p2(filename):
     for line in open(filename):
