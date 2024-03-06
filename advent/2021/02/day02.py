e4548294484a4e2594416f703cee96738ad44c45
Cody Shitagi <codyts@hawaii.edu>
2024-03-05 22:19:03 -1000

    advent 2021 day 2

--- advent/2021/02/day02.py
+++ advent/2021/02/day02.py
@@ -15,8 +15,36 @@ class day02:
 
         print("answer for part 1\n" + str(self.h * self.d))
 
+def data(filename):
+    return ((line.split()[0], int(line.split()[1])) for line in open(filename))
+def part2(filename):
+    operations = data(filename)
+    depth = 0
+    horizontal_position = 0
+    
+    aim = 0
+    for i, op in enumerate(operations):
+        print(op)
+        if op[0] == "forward":
+            depth += aim * op[1]
+            horizontal_position += op[1]
+            print(i, aim, horizontal_position)
+        elif op[0] == "up":
+            aim -= op[1]
+            print(i, aim, "up")
+        elif op[0] == "down":
+            aim += op[1]
+            print(i, aim, "down")
+
+    print("Horizontal Position: " + str(horizontal_position))
+    print("Depth: " + str(depth))
+    print("The answer to part 2: ")
+    print(depth * horizontal_position)
+
+
+
+
 #day02("example").part1()
 day02("input").part1()
 
-                
-
+part2("input")
